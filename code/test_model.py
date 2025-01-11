f9263f6941ca51a6366049a9900e2bc42aabd721
Buddanna T <you@example.com>
2025-01-12 00:46:36 +0530

    Resolved the lint issues

--- code/test_model.py
+++ code/test_model.py
@@ -1,15 +1,17 @@
-#import the required libraries
+# import the required libraries
 import joblib
 import os
 
-#Load the model using joblib
+# Load the model using joblib
 model_path = os.path.join('model', 'model.joblib')
 my_model = joblib.load(model_path)
 
-#Test model loaded or not
+
+# Test model loaded or not
 def test_model_load():
     assert my_model is not None, "Failed to load the saved model."
 
-#Test the prediction for two values
+
+# Test the prediction for two values
 def test_predict():
-    assert my_model.predict([[6.8,2.8,4.8,1.4]]) == 1
+    assert my_model.predict([[6.8, 2.8, 4.8, 1.4]]) == 1
