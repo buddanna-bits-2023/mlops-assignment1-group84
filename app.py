96e8f820fee190e2133dba4c512de304833aa48d
Buddanna T <you@example.com>
2025-01-12 23:43:02 +0530

    Hyper tuned the mdel with GridSearchCV and containerized with Flask API

---
+++ app.py
@@ -0,0 +1,28 @@
+#import required libraries
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+import os
+
+#Create Flask API instance
+app = Flask(__name__)
+
+#Load the best performance ML model from the joblib file
+model_path = os.path.join('model', 'hyper_tuned_model.joblib')
+model = joblib.load(model_path)
+
+#Default route on launching the Flask API
+@app.route('/', methods=['GET'])
+def default():
+    return 'Flask API running successfully.'
+
+#API POST method/route for prediction of given inputs
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    prediction = model.predict(np.array(data['input']).reshape(1,-1))
+    return jsonify({'prediction': prediction.tolist()})
+
+#Starting the Flask API
+if __name__ == '__main__':
+    app.run(debug=True)
